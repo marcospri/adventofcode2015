f17708febaa82b72e65f499cce37302fb69e30c4
Marcos Prieto <marcospri@gmail.com>
2015-12-17 22:26:58 +0100

    Day 17, part 2

--- day17/solver.exs
+++ day17/solver.exs
@@ -5,15 +5,20 @@ defmodule Day17 do
       (for y <- combination(n - 1, xs), do: [x|y]) ++ combination(n, xs)
   end
 
-  def find_bottles _, 0, n_combinations do n_combinations end
-  def find_bottles bottles_sizes, n_bottles, n_combinations do
+  def find_bottles(_, n_bottles, total_bottles) when n_bottles > total_bottles do 0 end
+  def find_bottles bottles_sizes, n_bottles, total_bottles do
     total = combination(n_bottles, bottles_sizes)
     |> Enum.filter(fn(x) -> Enum.sum(x) == 150 end)
     |> Enum.count
-    find_bottles bottles_sizes,  n_bottles - 1, n_combinations + total
+    case total > 0 do
+      true ->
+        total
+      false ->
+        find_bottles bottles_sizes,  n_bottles + 1, total_bottles
+    end
   end
   def find_bottles bottles do
-    find_bottles bottles, Enum.count(bottles), 0
+    find_bottles bottles, 2, Enum.count(bottles)
   end
 
 end
